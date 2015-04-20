8e025200533f0fbb2dd92da08945594f13b78a90
Steve Phillips <StevePhillipsCa@gmail.com>
2015-04-20 13:49:36 -0700

    Added mouseleave to spans that are active

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,14 +6,25 @@ class CommentsController < ApplicationController
 			}
 		end
 	end
+	def destroy
+		Comment.destroy(params[:id])
+	end
 	def moderate
 		@all_comments = Comment.all
 	end
 	def edit
+		# I'm naming things the same way wordpress's c-style api does
+		# prefixing 'the' in front of things
 		@the_comment = Comment.find(params[:id])
+		@the_post = Post.find(@the_comment.post_id)
 	end
 	def preview
 		print 'the preview method is called'
+		@the_comment = Comment.find(params[:id])
+	end
+	def update
+		Comment.find(params[:id]).update(comment_params)
+		redirect_to :action=>'moderate'
 	end
 	attr_reader :all_comments
 private
